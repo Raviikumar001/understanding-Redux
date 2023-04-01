7adca9824655e8c4b8ab6e5ee029a13978e22258
ravi <samcollins9899@gmail.com>
2023-04-02 00:16:55 +0530

    added reducer

---
+++ When we are making our app, we want our .md
@@ -0,0 +1,21 @@
+When we are making our app, we want our state to be managed well
+so what we can do to make our state more predictable is to have a
+central state.
+What we want to achieve is state predictabiliy when we are making our app
+then if the state is modified by anyone then the state predictiablity decreses
+and therefore we cannot predict state.
+
+A) One way is we can make an object that describes our state. We can call it an action.
+Action is a object which represent an specific event , which will change the state of our store
+We use pure functions to solve this. Pure function are fuctions which do not change the state
+but return them as it is.
+
+Characteristics of Pure Function
+
+1. The always return the same result if the same arguments are passed in.
+2.) They depend on the arguments passed into them. The return value of a pure function is <!-- 
+   depedent on the input it receives and nothing else.  -->
+3.Pure Functions must never produce any side effects.
+
+
+Reducer takes the state and reduces it to simple actions.
--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-function createStore()
-{
+function todos(state = [], action) {
+  if (action.type === "ADD_TODO") {
+    return state.concat([action.todo]);
+  }
 
-    let store;
-    let listeners= []
-
-    let subscribe = (listener)=> {
-        listeners.push(listener);
-
-        return ()=>{
-            listeners = listeners.filter( (l)=> l!==listener);
-        }
-    }
-
-
-    return{
-        store,
-        subscribe
-    }
+  return state;
 }
+
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  const getstate = () => {
+    return state;
+  };
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    //call todos
+    //loop over listeners and invoke them
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getstate,
+    subscribe,
+    dispatch,
+  };
+}
+
+const store = createStore(todos);
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false,
+  },
+});
