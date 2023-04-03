c3ddadd489900c80ab1713f90f7a110a1c65117d
ravi <samcollins9899@gmail.com>
2023-04-03 19:02:56 +0530

    added switch action

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function todos(state = [], action) {
-  if (action.type === "ADD_TODO") {
-    return state.concat([action.todo]);
-  }else if (action.type === "REMOVE_TODO")
-  {
-    return state.filter( (todo)=> todo.id !== action.id)
-  }else if (action.type === "")
 
-  return state;
+  switch(action.type)
+  {
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+    case 'REMOVE_TODO':
+      return state.filter( (todo)=> todo.id !== action.id);
+    case 'TOGGLE_TODO':
+      return state.map((todo)=> todo.id !== action.id
+      ?todo
+      :Object.assign({},todo, {complete: !todo.complete}))
+    default: 
+      return state;
+  }
+  
+
+
+
 }
 
 function createStore(reducer) {
