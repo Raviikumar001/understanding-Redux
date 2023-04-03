6a0797a8d85decd5b0c280239067ae3f455e0e36
ravi <samcollins9899@gmail.com>
2023-04-03 12:13:07 +0530

    added actions

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function todos(state = [], action) {
   if (action.type === "ADD_TODO") {
     return state.concat([action.todo]);
-  }
+  }else if (action.type === "REMOVE_TODO")
+  {
+    return state.filter( (todo)=> todo.id !== action.id)
+  }else if (action.type === "")
 
   return state;
 }
