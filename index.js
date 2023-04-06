a4059fe8be603118a45b4d2357c1fbe6ca2ab902
ravi <samcollins9899@gmail.com>
2023-04-06 13:13:03 +0530

    added state to ui

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function generateID()
+{
+   return Math.random().toString(36).substring(2) + (new Date().getTime().toString(36));
+}
+
+
 function createStore(reducer)
 {
 
@@ -7,7 +13,7 @@ function createStore(reducer)
   const getState= ()=>{
     return state;
   }
-  
+
   const subscribe = (listener)=>{
 
     listeners.push(listener);
@@ -136,41 +142,81 @@ store.subscribe( ()=> {
   console.log( "The new state is :", store.getState());
 }) 
 
-store.dispatch(addTodoAction({
-  id:0,
-  name: "walk the dog",
-  complete:false
-}))
+// store.dispatch(addTodoAction({
+//   id:0,
+//   name: "walk the dog",
+//   complete:false
+// }))
 
-store.dispatch(addTodoAction({
-  id: 1,
-  name: 'Make maggie',
-  complete: false
-}))
-
-
+// store.dispatch(addTodoAction({
+//   id: 1,
+//   name: 'Make maggie',
+//   complete: false
+// }))
 
 
-store.dispatch(removeTodoAction(1))
 
-store.dispatch(toggleTodoAction(0))
 
-store.dispatch(addGoalAction( {
-  id: 0,
-  name: "Learn Redux"
-}
-))
+// store.dispatch(removeTodoAction(1))
 
-store.dispatch(addGoalAction( {
-  id: 1,
-  name: "Learn Dancing"
-}
-))
+// store.dispatch(toggleTodoAction(0))
 
-store.dispatch(addGoalAction({
-  id: 1,
-  name: "Learn React"
-}))
+// store.dispatch(addGoalAction( {
+//   id: 0,
+//   name: "Learn Redux"
+// }
+// ))
+
+// store.dispatch(addGoalAction( {
+//   id: 1,
+//   name: "Learn Dancing"
+// }
+// ))
+
+// store.dispatch(addGoalAction({
+//   id: 1,
+//   name: "Learn React"
+// }))
 
 
 store.dispatch(removeGoalAction(0))
+
+
+//DOM CODE
+
+function Addtodo()
+{
+    
+  const input = document.getElementById('todo');
+  const name =  input.value;
+  input.value= '';
+
+  store.dispatch(addTodoAction(
+      {
+        id: generateID(),
+        name,
+        complete:false, 
+      }  ))
+  
+}
+
+function AddGoal()
+{
+  const input = document.getElementById('goal');
+  const name = input.value;
+  input.value = ""
+
+  store.dispatch(addGoalAction(
+      {
+          id:generateID(),
+          name,
+          complete:false
+      }
+  ))
+}
+
+document.getElementById('todobtn')
+.addEventListener("click",Addtodo)
+
+document.getElementById('goalBtn')
+.addEventListener("click",AddGoal)
